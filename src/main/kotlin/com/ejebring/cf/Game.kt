1f9c4c8e502601c04c07cf05cfc9360068f8856d
eejebring <dev@ejebring.com>
2024-12-05 19:41:42 +0100

    Game fix

---
+++ src/main/kotlin/com/ejebring/cf/Game.kt
@@ -0,0 +1,37 @@
+package com.ejebring.cf
+
+// Board is a string of 42 characters, each representing a cell in the Connect Four board. y for yellow and r for red n for none.
+class Game(
+    val redPlayer: String,
+    val yellowPlayer: String,
+    var redPlayedLast: Boolean = true,
+    var isFinished: Boolean = false,
+    var board: String = "n".repeat(42)
+) {
+    fun playMove(column: Int, playerName: String) {
+        val currentPlayer = if (redPlayedLast) yellowPlayer else redPlayer
+
+        if (isFinished) {
+            throw IllegalArgumentException("Game is already finished")
+        }
+        if (playerName != currentPlayer) {
+            throw IllegalArgumentException("It is not your turn")
+        }
+        if (column < 0 || column > 6) {
+            throw IllegalArgumentException("Column must be between 0 and 6")
+        }
+        if (board[column] != 'n') {
+            throw IllegalArgumentException("Column is full")
+        }
+
+        for (row in intArrayOf(35, 28, 21, 14, 7, 0)) {
+            val index = row + column
+            val currentPiece = if (redPlayedLast) 'r' else 'y'
+            if (board[index] == 'n') {
+                board = board.substring(0, index) + currentPiece + board.substring(index + 1)
+                return
+            }
+        }
+        redPlayedLast = !redPlayedLast
+    }
+}
--- src/main/kotlin/com/ejebring/cf/GameSchema.kt
+++ src/main/kotlin/com/ejebring/cf/GameSchema.kt
@@ -14,42 +14,6 @@ object DBGame : Table() {
     override val primaryKey = PrimaryKey(id)
 }
 
-// Board is a string of 42 characters, each representing a cell in the Connect Four board. y for yellow and r for red n for none.
-class Game(
-    val redPlayer: String,
-    val yellowPlayer: String,
-    var redPlayedLast: Boolean = true,
-    var isFinished: Boolean = false,
-    var board: String = "n".repeat(42)
-) {
-    fun playMove(column: Int, playerName: String) {
-        val currentPlayer = if (redPlayedLast) yellowPlayer else redPlayer
-
-        if (isFinished) {
-            throw IllegalArgumentException("Game is already finished")
-        }
-        if (playerName != currentPlayer) {
-            throw IllegalArgumentException("It is not your turn")
-        }
-        if (column < 0 || column > 6) {
-            throw IllegalArgumentException("Column must be between 0 and 6")
-        }
-        if (board[column] != 'n') {
-            throw IllegalArgumentException("Column is full")
-        }
-
-        for (row in 0..5) {
-            val index = row * 7 + column
-            val currentPiece = if (redPlayedLast) 'r' else 'y'
-            if (board[index] == 'n') {
-                board = board.substring(0, index) + currentPiece + board.substring(index + 1)
-                return
-            }
-        }
-        redPlayedLast = !redPlayedLast
-    }
-}
-
 class GameSchema(database: Database) {
 
     init {
