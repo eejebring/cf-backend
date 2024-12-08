5e93f02fa106af307707dff415096a77333292e1
eejebring <dev@ejebring.com>
2024-12-08 16:46:07 +0100

    First turn for the challengee

--- src/main/kotlin/com/ejebring/cf/routes/Challenge.kt
+++ src/main/kotlin/com/ejebring/cf/routes/Challenge.kt
@@ -36,7 +36,7 @@ suspend fun challenge(
 
     if (challengeService.getUserChallenges(challenger).any { it.challenger == challenged }) {
         challengeService.remove(challenge)
-        gameSchema.create(Game(challenger, challenged))
+        gameSchema.create(Game(challenged, challenger))
         call.respond(HttpStatusCode.OK, "Challenge accepted")
         return
     }
